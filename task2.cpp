66e44e34595b836fcfef776fe972c274af02c264
OneCatGamer <kekaterinushkin@mail.ru>
2022-05-07 20:55:44 +0300

    Edited function fib(n) with counting from 0, 1 ...

--- task2.cpp
+++ task2.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int f(int n) {
-    if (n == 1 || n == 2)
+    if (n == 2)
         return 1;
-    if (n == 0)
+    if (n == 0 || n == 1)
         return 0;
     return f(n - 1) + f(n - 2);
 }
